fb57b4f28fc9a971da06b6f31bbdd30fe8f794de
pritisudha.pattanaik@spire2grow.com <pritisudha.pattanaik@spire2grow.com>
2016-08-10 10:30:19 +0530

    commiting candidatenotesconsumer,testplan,service end point properties.

--- gi/src/main/java/com/spire/service/consumers/CandidateNotesConsumers.java
+++ gi/src/main/java/com/spire/service/consumers/CandidateNotesConsumers.java
@@ -4,6 +4,7 @@ import javax.ws.rs.core.Response;
 
 
 
+
 import org.apache.http.impl.client.BasicResponseHandler;
 
 import com.spire.base.controller.Logging;
@@ -13,13 +14,10 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 {
 	String endPointURLCandidatenoteslist = getServiceEndPoint("CANDIDATE_NOTES_LIST");
 	String endPointURLCandidatenotessearch=getServiceEndPoint("CANDIDATE_NOTES_SEARCH");
-	String endPointURLCandidatenoteswithoutinterval=getServiceEndPoint("CANDIDATE_NOTES_LIST_WITHOUT_INTERVAL");
-	String endpointURLCandidatenotelistwithoutentityid=getServiceEndPoint("CANDIDATE_NOTES_LIST_WITHOUT_ENTITYID");
-	String endpointURLCandidatenotesearchwithoutsearchtext=getServiceEndPoint("CANDIDATE_NOTES_SEARCH_WITHOUT_SEARCHTEXT");
-	String endpointURLCandidatenotesearchwithoutentityid=getServiceEndPoint("CANDIDATE_NOTES_SEARCH_WITHOUT_ENTITYID");
+	
 	
 	public Response getCandidatenoteslist(String hostName) {
-		String serviceEndPoint = endPointURLCandidatenoteslist.replaceAll("hostAddress", hostName);
+		String serviceEndPoint = endPointURLCandidatenoteslist.replaceAll("hostAddress", hostName)+"?entityId=Testing1&interval=1";
 		System.out.println(" EndPoint URL >>" + serviceEndPoint);
 		Logging.log(" EndPoint URL >>" + serviceEndPoint);
 		Response response = executeGET(serviceEndPoint);
@@ -28,7 +26,7 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 		return response;
 	}
 	public Response getCandidatenotesearch(String hostName) {
-		String serviceEndPoint =endPointURLCandidatenotessearch.replaceAll("hostAddress", hostName);
+		String serviceEndPoint =endPointURLCandidatenotessearch.replaceAll("hostAddress", hostName)+"?entityId=Testing1&searchText=candidate%20selected";
 		System.out.println(" EndPoint URL >>" + serviceEndPoint);
 		Logging.log(" EndPoint URL >>" + serviceEndPoint);
 		Response response = executeGET(serviceEndPoint);
@@ -39,7 +37,8 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 	
 	public Response getCandidatenoteslistwithoutInterval(String hostName)
 	{
-		String serviceEndPoint =endPointURLCandidatenoteswithoutinterval.replaceAll("hostAddress", hostName);
+		//String serviceEndPoint = lookUp.replaceAll("hostAddress", hostName)+"?type=REQUISITION_STATUS";
+		String serviceEndPoint =endPointURLCandidatenoteslist.replaceAll("hostAddress", hostName)+"?entityId=Testing1";
 		System.out.println(" EndPoint URL >>" + serviceEndPoint);
 		Logging.log(" EndPoint URL >>" + serviceEndPoint);
 		Response response = executeGET(serviceEndPoint);
@@ -50,7 +49,7 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 	}
 	public Response getCandidatenoteslistwithoutEntityid(String hostName)
 	{
-		String serviceEndPoint =endpointURLCandidatenotelistwithoutentityid.replaceAll("hostAddress", hostName);
+		String serviceEndPoint =endPointURLCandidatenoteslist.replaceAll("hostAddress", hostName)+"?interval=1";
 		System.out.println(" EndPoint URL >>" + serviceEndPoint);
 		Logging.log(" EndPoint URL >>" + serviceEndPoint);
 		Response response = executeGET(serviceEndPoint);
@@ -62,7 +61,7 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 	
 	public Response getCandidatenotessearchwithoutsearchtext(String hostName)
 	{
-		String serviceEndPoint =endpointURLCandidatenotesearchwithoutsearchtext.replaceAll("hostAddress", hostName);
+		String serviceEndPoint =endPointURLCandidatenotessearch.replaceAll("hostAddress", hostName)+"?entityId=Testing1";
 		System.out.println(" EndPoint URL >>" + serviceEndPoint);
 		Logging.log(" EndPoint URL >>" + serviceEndPoint);
 		Response response = executeGET(serviceEndPoint);
@@ -74,7 +73,7 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 	}
 	public Response getCandidatenotessearchwithoutentityid(String hostName)
 	{
-		String serviceEndPoint =endpointURLCandidatenotesearchwithoutentityid.replaceAll("hostAddress", hostName);
+		String serviceEndPoint =endPointURLCandidatenotessearch.replaceAll("hostAddress", hostName)+"?searchText=java";
 		System.out.println(" EndPoint URL >>" + serviceEndPoint);
 		Logging.log(" EndPoint URL >>" + serviceEndPoint);
 		Response response = executeGET(serviceEndPoint);
@@ -83,6 +82,29 @@ public class CandidateNotesConsumers extends BaseServiceConsumerNew
 		return response;
 		
 		
+	}
+	
+	public Response getCandidatenotelistwithoutanyparameter(String hostname)
+	{
+		String serviceEndPoint =endPointURLCandidatenoteslist.replaceAll("hostAddress", hostname);
+		System.out.println(" EndPoint URL >>" + serviceEndPoint);
+		Logging.log(" EndPoint URL >>" + serviceEndPoint);
+		Response response = executeGET(serviceEndPoint);
+		System.out.println("RESPONSE CODE >>" + response.getStatus());
+		Logging.log("Response Code >>"+response.getStatus());
+		return response;
+		
+		
+	}
+	public Response getCandidatenotesearchwithoutanyparameter(String hostname)
+	{
+		String serviceEndPoint =endPointURLCandidatenotessearch.replaceAll("hostAddress", hostname);
+		System.out.println(" EndPoint URL >>" + serviceEndPoint);
+		Logging.log(" EndPoint URL >>" + serviceEndPoint);
+		Response response = executeGET(serviceEndPoint);
+		System.out.println("RESPONSE CODE >>" + response.getStatus());
+		Logging.log("Response Code >>"+response.getStatus());
+		return response;
 	}
 
 }
