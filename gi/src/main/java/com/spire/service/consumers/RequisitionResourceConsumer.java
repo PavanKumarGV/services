3b985368f93d99a6805b78bc8a42398c6439d85e
unknown <udhayamurthy.alwar@spire2grow.com>
2016-08-10 16:53:36 +0530

    Requisition search testcase

---
+++ gi/src/main/java/com/spire/service/consumers/RequisitionResourceConsumer.java
@@ -0,0 +1,41 @@
+package com.spire.service.consumers;
+
+import java.io.IOException;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.http.client.ClientProtocolException;
+import org.testng.Assert;
+
+import com.spire.base.controller.Logging;
+import com.spire.base.service.BaseServiceConsumerNew;
+
+public class RequisitionResourceConsumer extends BaseServiceConsumerNew {
+	
+	String endPointURL_REQ = getServiceEndPoint("REQUISITION_SEARCH");
+	
+	
+	public RequisitionResourceConsumer(String username, String password, String hostName) {
+		Logging.log("Inside of Login");
+		       System.out.println("Inside of Login");
+		getUserToken(username, password, hostName);
+		}
+
+	
+	public void getRequisition(String hostName)
+			throws ClientProtocolException, IOException {
+		String serviceEndPoint = endPointURL_REQ.replaceAll("hostAddress",
+				hostName);
+		System.out.println(" EndPoint URL >>" + serviceEndPoint);
+		Response response1 = executeGET(serviceEndPoint);
+		if (response1.getStatus() == 200) {
+			System.out.println("********** pass **************");
+		} else {
+			Assert.fail();
+		}
+
+	}
+
+}
+
+
