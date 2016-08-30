c10c927a3447a736d72acb2c1e858e0d3d6b9fb2
Rajasekhar <rajasekhar.reddy@spire2grow.com>
2016-08-30 12:45:25 +0530

    To test in 192.168.2.202 machine

--- gi/src/main/java/com/spire/base/service/Constants.java
+++ gi/src/main/java/com/spire/base/service/Constants.java
@@ -1,13 +1,13 @@
 package com.spire.base.service;
 
 public class Constants {
-	public static final String candidate_Id = "6030:6005:a85ee5e026be4046b9a7116e580fd639";
-	public static final String candidate_Id1 = "6079:6005:c3dbcb92dd8e4fd29af671d09d79b4fd";
-	public static final String requisition_Id = "6030:6066:ef7a79270ffd40ae93adc4cfc5b9da7b";
-	public static final String requisition_Id1 = "6079:6066:055c8bc96fe34ef6b3c036b2136d4d20";
+	public static final String candidate_Id = "6100:6005:9b1106829ee94d91b38f8e14e1aa3ca0";
+	public static final String candidate_Id1 = "6100:6005:bb17361fac2a46978169b746b3cafe9e";
+	public static final String requisition_Id = "6100:6066:846ba741d5184547b136eaa2af13835a";
+	public static final String requisition_Id1 = "6100:6066:ed8d850ce3c044bba5fd90c25fcd00ad";
 	public static final String candidate_Id2 = "6079:6005:c3dbcb92dd8e4fd29af671d09d79b4fd";
-	public static final String user_Id = "user@uat.com";
-	public static final String password = "spire@123";
+	public static final String user_Id = "sekhar@accenturebpodev.com";
+	public static final String password = "iVkzceza";
 	public static final String partial_Skill_to_search="Ja";
     public static final String full_skill_to_search="Java%20Script";
     public static final String partial_institute_to_search="UNIVERSITY";
@@ -33,7 +33,7 @@ public class Constants {
     public static final String sourcename="sourcename";
     public static final String status="status";
     public static final String candidate_resume="6079:6005:09c8cccc8ea74ad7a890475063bec46e";
-    public static final String changeStats="6079:6066:28ec518324ed4d8b9562b2b6ffb72804";
+    public static final String changeStats="6100:6005:9b1106829ee94d91b38f8e14e1aa3ca0";
     public static final String test_status ="Test";
     public static final String test_status1 = "Test1";
     public static final String candidate_deallocate = "6079:6005:25ae5f1585aa4f409080c4a09eabc26b";
