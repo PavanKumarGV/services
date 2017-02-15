c633c7882e1d352024d28d61ccbb5d9dee7227aa
Garnepudi V <Garnepudi V@172.16.1.28>
2017-02-15 13:17:13 +0530

    Included change to dynamically pick-up config and test data csv files for specific client.

--- gi/src/test/java/com/spire/tests/AnalyticServiceTest.java
+++ gi/src/test/java/com/spire/tests/AnalyticServiceTest.java
@@ -1,4 +1,4 @@
-package com.spire.analyticservices;
+package com.spire.tests;
 
 import java.lang.reflect.Method;
 import java.util.Iterator;
@@ -16,24 +16,44 @@ import com.spire.base.service.utils.PropertiesPicker;
 import com.spire.base.util.SpireCsvUtil;
 import com.spire.base.util.internal.entity.SpireTestObject;
 
-public class TestAnalyticService extends TestPlan {
+public class AnalyticServiceTest extends TestPlan {
 
+	public static String clientName = null;
 	public static String environment = null;
 	public static String filePathForAnalytics = null;
+	public static String fileName = null;
+
+	@Parameters({ "env", "client" })
+	@BeforeClass
+	public static void init(String env, String client) {
+
+		environment = env;
+		clientName = client;
+
+		System.out.println("Env being used" + " " + environment);
+		System.out.println("Client app for which tests being run is" + " " + clientName);
+
+		fileName = "./src/test/java/com/spire/tests/" + clientName + "TestData.csv";
+		filePathForAnalytics = "./src/test/java/com/spire/tests/" + clientName + ".properties";
+
+		System.out.println("CSV file with test Data is" + " " + fileName);
+		System.out.println("Properties file with config details" + " " + filePathForAnalytics);
+	}
 
 	@DataProvider(name = "AnalyticsTestData")
+
 	public static Iterator<Object[]> gettestData(Method method) {
 
 		Iterator<Object[]> objectsFromCsv = null;
 
 		try {
-			String fileName = "./src/test/java/com/spire/analyticservices/AnalyticsTestData.csv";
+
 			LinkedHashMap<String, Class<?>> entityClazzMap = new LinkedHashMap<String, Class<?>>();
 			LinkedHashMap<String, String> methodFilter = new LinkedHashMap<String, String>();
 			methodFilter.put(SpireTestObject.TEST_TITLE, method.getName());
 			entityClazzMap.put("SpireTestObject", SpireTestObject.class);
 			entityClazzMap.put("AnalyticsDataSet", AnalyticsDataSet.class);
-			objectsFromCsv = SpireCsvUtil.getObjectsFromCsv(TestAnalyticService.class, entityClazzMap, fileName, null,
+			objectsFromCsv = SpireCsvUtil.getObjectsFromCsv(AnalyticServiceTest.class, entityClazzMap, fileName, null,
 					methodFilter);
 
 		} catch (Exception e) {
@@ -42,19 +62,22 @@ public class TestAnalyticService extends TestPlan {
 		return objectsFromCsv;
 	}
 
-	@Parameters({ "env" })
-	@BeforeClass	 
-	public static void init(String env) {
+	@Test(groups = { "sanity" }, dataProvider = "AnalyticsTestData")
+	public void testAnalyticsService(SpireTestObject testObject, AnalyticsDataSet testData) {
 
-		filePathForAnalytics = "./src/test/java/com/spire/analyticservices/Analytic_Service.properties";
-		System.out.println("Env from @Parameter" + " " + env);
-
-		environment = env;
+		AnalyticsRestClient<AnalyticsDataSet> utility = new AnalyticsRestClient<AnalyticsDataSet>(environment,
+				filePathForAnalytics);
+		int respValidate[] = utility.postServiceResp(PropertiesPicker.getValues(filePathForAnalytics, "reqBody"),
+				testData);
+		for (int i = 0; i < respValidate.length; i++) {
+			Assert.assertEquals(respValidate[0] == 200, true, "Service response is not true");
+			Assert.assertEquals(respValidate[1] > 0, true, "Service response has results");
+		}
 
 	}
 
-	@Test(groups = { "sanity" }, dataProvider = "AnalyticsTestData")
-	public void testAnalyticsService(SpireTestObject testObject, AnalyticsDataSet testData) {
+	@Test(groups = { "Regression" }, dataProvider = "AnalyticsTestData")
+	public void testAnalyticsServiceReg(SpireTestObject testObject, AnalyticsDataSet testData) {
 
 		AnalyticsRestClient<AnalyticsDataSet> utility = new AnalyticsRestClient<AnalyticsDataSet>(environment,
 				filePathForAnalytics);
